eb54ad6aefff63e3f071efdff82b825cffe46bfb
Hanserprpr <2041283903@qq.com>
2026-03-24 01:14:50 +0800

    fix: apply result mappings for restaurant search

--- src/main/java/you/v50to/eatwhat/mapper/RestaurantMapper.java
+++ src/main/java/you/v50to/eatwhat/mapper/RestaurantMapper.java
@@ -4,12 +4,24 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import you.v50to.eatwhat.data.po.Restaurant;
+import you.v50to.eatwhat.utils.PointTypeHandler;
+import you.v50to.eatwhat.utils.StringArrayTypeHandler;
+import you.v50to.eatwhat.utils.TimestampTypeHandler;
 
 @Mapper
 public interface RestaurantMapper extends BaseMapper<Restaurant> {
 
+    @Results(id = "restaurantResultMap", value = {
+            @Result(property = "POI", column = "poi"),
+            @Result(property = "location", column = "location", typeHandler = PointTypeHandler.class),
+            @Result(property = "pictureUrl", column = "picture_url", typeHandler = StringArrayTypeHandler.class),
+            @Result(property = "createdAt", column = "created_at", typeHandler = TimestampTypeHandler.class),
+            @Result(property = "updatedAt", column = "updated_at", typeHandler = TimestampTypeHandler.class)
+    })
     @Select("""
             SELECT *,
                    ST_Distance(location, ST_SetSRID(ST_MakePoint(#{wgsLng}, #{wgsLat}), 4326)::geography) AS distance
